1029d9a095f07271051f09e55d7a83480cc67a0e
iamwinner422 <iamwinner422@gmail.com>
2022-06-23 13:42:12 +0000

    Ajout des relations dans les models respectifs

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Post;
 class Category extends Model
 {
     use HasFactory;
+    /*RELATION: UNE CATEGORY A PLUSIEURS POSTS*/
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\User;
+use App\Models\Category;
 class Post extends Model
 {
     use HasFactory;
+    /*RELATION UN POST APPARTIENT A UN UTILISATEUR*/
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    /*RELATION UN POST APPARTIENT A UNE CATEGORY*/
+    public function category(){
+        return $this->belongsTo(User::class);
+    }
 }
