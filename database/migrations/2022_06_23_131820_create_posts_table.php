f814e8ddc400269cff5d3ee362801e86b2f8cc3c
iamwinner422 <iamwinner422@gmail.com>
2022-06-23 13:28:53 +0000

    Ajout des différents champs dans la table Post

--- database/migrations/2022_06_23_131820_create_posts_table.php
+++ database/migrations/2022_06_23_131820_create_posts_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use App\Models\Category;
 
 class CreatePostsTable extends Migration
 {
@@ -15,7 +17,14 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('content');
+            $table->string('image');
             $table->timestamps();
+
+            /*CREATION DE CLE ETRANGERE*/
+            $table->foreignIdFor(User::class);/*BELONGS TO USER*/
+            $table->foreignIdFor(Category::class);/*BELONGS TO CATEGORY*/
         });
     }
 
